cb6a2a3599eedc841e07202f3e9d15943096c684
tomselden <88065752+tomselden@users.noreply.github.com>
2022-02-07 17:59:23 -0500

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,17 @@
-import GalleryItem from './GalleryItem'
+import React, { Suspense } from "react";
 
-function Gallery(props){
+function Gallery(props) {
+  const GalleryItem = React.lazy(() => import("./GalleryItem"));
 
-    const display = props.data.map((item, index) => {
-        return (
-            <GalleryItem item={item} key={index} />
-        )
-    })
-
+  const display = props.data.map((item, index) => {
     return (
-        <div>
-            {display}
-        </div>
-    )
+      <Suspense fallback={<h1>Getting Music Data...</h1>}>
+        <GalleryItem item={item} key={index} />
+      </Suspense>
+    );
+  });
+
+  return <div>{display}</div>;
 }
 
-export default Gallery
+export default Gallery;
